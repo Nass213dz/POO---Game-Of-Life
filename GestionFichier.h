6110a2e9ecdf18016a53aa517c3ce637c35c5ff1
Nassim.bms <158268671+Nass213dz@users.noreply.github.com>
2024-11-26 16:15:02 +0100

    Add files via upload

---
+++ GestionFichier.h
@@ -0,0 +1,9 @@
+#ifndef GESTIONFICHIER_H
+#define GESTIONFICHIER_H
+
+#include <string>
+
+std::string lireFichier(const std::string& nomFichier);
+void ecrireFichier(const std::string& nomFichier, const std::string& contenu);
+
+#endif
