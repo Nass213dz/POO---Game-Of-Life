4f9b4922338bfe57ba55e7ad29ebba9eb477a339
Nassim.bms <158268671+Nass213dz@users.noreply.github.com>
2024-11-27 14:41:35 +0100

    Delete GestionFichier.h

--- GestionFichier.h
+++
@@ -1,9 +0,0 @@
-#ifndef GESTIONFICHIER_H
-#define GESTIONFICHIER_H
-
-#include <string>
-
-std::string lireFichier(const std::string& nomFichier);
-void ecrireFichier(const std::string& nomFichier, const std::string& contenu);
-
-#endif
