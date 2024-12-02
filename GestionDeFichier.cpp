bd6d3370bd1ca71f0fdf8524bf070b1f60bf020b
Nass213dz <nassouben44@gmail.com>
2024-12-02 09:53:47 +0100

    normalement la partie console est finie

--- GestionDeFichier.cpp
+++ GestionDeFichier.cpp
@@ -42,33 +42,37 @@ vector<vector<int>> GestionDeFichier::lireFichier(const string& nomFichier) {
         //continue de parcourir la première ligne
         if (premiereLigne[j] != ' '){
             //si il y a un autre élément
-            throw runtime_error("Declaration du tableau invalide."); // message d'erreur
+            throw runtime_error("Declaration du tableau invalide (pensez à verifier qu'il n'y a pas de caractères en trop)"); // message d'erreur
         }
     }
     largeurI = stoi(largeurS); // conversion de la chaîne de caractère en entier
     longueurI = stoi(longueurS);
 
     vector<vector<int>> tableau; //vecteur d'entiers appelé tableau
-    int x(0);
+    int x=0;
 
     //remplissage du tableau
     while(getline(lireMessage, ligne)){
         tableau.push_back(vector<int>()); // Ajoute une nouvelle ligne vide
         for (int i(0); i<ligne.size(); i += 2){
-            int cellule = ligne[i] - '0';
-            tableau[x].push_back(cellule);
+            //parcourt la ligne de 2 en 2 pour éviter les espaces
+            int cellule = ligne[i] - '0'; //transforme le caractère en int à l'aide de la table ASCII
+            tableau[x].push_back(cellule); //on ajoute la valeur de cellule dans le vecteur tableau (à l'indice x) 
         }
         x++;
     }
-    lireMessage.ignore();
+    lireMessage.ignore(); //ignore les caractères pouvant fausser l'analyse
 
     if (tableau.size() != largeurI){
-        throw runtime_error("Nombre de lignes de la matrice non conforme aux parametres donnes");
+        //si la taille du tableau ne correspond pas à la largeur entrée en paramètre
+        throw runtime_error("Nombre de lignes de la matrice non conforme aux parametres donnes"); //message d'erreur
     }
 
     for (int i = 0; i < tableau.size(); i++){
+        //parcourt de chaque ligne
         if (tableau[i].size() != longueurI){
-            throw runtime_error("Nombre de colonnes de la matrice non conforme aux parametres donnes");
+            //si la taille du tableau ne correspond pas à la longueur entrée en paramètre
+            throw runtime_error("Nombre de colonnes de la matrice non conforme aux parametres donnes"); //message d'erreur
         }   
     }
        
@@ -76,19 +80,14 @@ vector<vector<int>> GestionDeFichier::lireFichier(const string& nomFichier) {
 }
 
 
-void GestionDeFichier::ecrireFichier(const string& nomFichier, const string& grille, int nb) {
+void GestionDeFichier::ecrireFichier(const string& nomFichier, const string& grille) {
     string iteration_folder;
     if (!fs::exists(iteration_folder)){
         fs::create_directory(iteration_folder);
     }
- 
-    for (int i(0); i < nb; i++){
-        ofstream fichier(nomFichier + (i+1));
-    }
-    
-    if (!fichier.is_open()) {
-        throw runtime_error("Impossible d'ouvrir le fichier de destination.");
-    }
-    fichier << contenu;
+     
+    string cheminFichier = iteration_folder + "/" + nomFichier;
+    ofstream fichier(cheminFichier);
+    fichier << grille;
     fichier.close();
 }
