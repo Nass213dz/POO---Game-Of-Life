77aa3fca242f088165f9e0e7767bc1625e43b27c
Nass213dz <nassouben44@gmail.com>
2024-12-04 12:35:43 +0100

    ffv

--- Grille.cpp
+++ Grille.cpp
@@ -14,7 +14,7 @@ const int gridHeight = 80;
 Grille::Grille(int largeur, int longueur) : m_largeur(largeur), m_longueur(longueur), cellules(longueur, vector<Cellule*>(largeur, nullptr)) {} // Constructeur paramétré
 
 Grille::~Grille() {
-    // Destructeur qui supprime chaque cellule du vecteur
+    //destructeur qui supprime chaque cellule du vecteur
     for (int i = 0; i < m_longueur; i++) {
         for (int j = 0; j < m_largeur; j++) {
             delete cellules[i][j];
@@ -23,42 +23,43 @@ Grille::~Grille() {
 }
 
 int Grille::getLargeur() const {
-    // Getter de la largeur
+    //getter de la largeur
     return m_largeur;
 }
 
 int Grille::getLongueur() const {
-    // Getter de la longueur
+    //getter de la longueur
     return m_longueur;
 }
 
 
 bool Grille::getVieCellule(int i, int j) const {
-    // Getter de l'état d'une cellule qui prend la position en paramètre
-    return cellules[i][j]->getEtat() ? true : false; // Si getEtat() retourne true, la méthode getVieCellule retourne true, sinon false
+    //getter de l'état d'une cellule à une position donnée en parametre
+    return cellules[i][j]->getEtat() ? true : false; //si getEtat() retourne true la méthode getVieCellule retourne true sinon false (condition ternaire)
 }
 
 void Grille::setVieCellule(int x, int y, bool vivante) {
+    //setter qui permet de changer l'état d'une cellule à une position prédéfinie
     if (x >= 0 && x < m_longueur && y >= 0 && y < m_largeur) {
-            cellules[x][y]->changerEtat(vivante); // Appelle changerEtat de la cellule
+            cellules[x][y]->changerEtat(vivante); //change l'état en vivant ou mort en fonction du booléen entré en paramètre
     }
 }
 
 int Grille::getNBRVoisinsVivants(int i, int j) {
-    // Permet d'avoir le nombre de voisins vivants autour de la cellule
-    int compteur_voisin_vivant = 0; // Initialisation du compteur
+    //calcule le nombre de voisins vivants autour de la cellule (à une position entrée en paramètre)
+    int compteur_voisin_vivant = 0; //init du compteur
 
     for (int k = -1; k <= 1; ++k) {
         for (int l = -1; l <= 1; ++l) {
-            // Permet de parcourir la grille (le vecteur cellules)
-            if (k == 0 && l == 0) continue; // Si le voisin parcouru est la cellule initiale, on continue sans rien faire
+            //parcourt les 8 voisins autour de la cellule
+            if (k == 0 && l == 0) continue; //skip la cellule initiale
 
-            int voisin_i = (i + k + m_longueur) % m_longueur; // Coordonnée en i d'un voisin
-            int voisin_j = (j + l + m_largeur) % m_largeur; // Coordonnée en j d'un voisin
+            int voisin_i = (i + k + m_longueur) % m_longueur; //coordonnée en i d'un voisin avec un modulo pour la grille torique
+            int voisin_j = (j + l + m_largeur) % m_largeur; //coordonnée en j d'un voisin avec un modulo pour la grille torique
 
-            if (voisin_i >= 0 && voisin_i < m_longueur && voisin_j >= 0 && voisin_j < m_largeur) { // Vérifie pour chaque voisin s'il est bien dans la grille
-                if (getVieCellule(voisin_i, voisin_j)) { // Si la cellule est vivante
-                    ++compteur_voisin_vivant; // Incrémenter le compteur
+            if (voisin_i >= 0 && voisin_i < m_longueur && voisin_j >= 0 && voisin_j < m_largeur) { //vérifie pour chaque voisin s'il est bien dans la grille
+                if (getVieCellule(voisin_i, voisin_j)) { //si la cellule est vivante
+                    ++compteur_voisin_vivant; //incrementation du compt
                 }
             }
         }
@@ -68,21 +69,23 @@ int Grille::getNBRVoisinsVivants(int i, int j) {
 }
 
 void Grille::affichageGrille() {
+    //affiche la grille
     cout << "Etat de la grille :" << endl;
-    for (int i = 0; i < m_longueur; i++) { // Parcours des lignes (longueur)
-        for (int j = 0; j < m_largeur; j++) { // Parcours des colonnes (largeur)
-            cout << (cellules[i][j]->getEtat() ? "1" : "0") << " "; // Affiche "1" pour vivant et "0" pour mort
+    for (int i = 0; i < m_longueur; i++) { // Parcours des lignes
+        for (int j = 0; j < m_largeur; j++) { // Parcours des colonnes
+            cout << (cellules[i][j]->getEtat() ? "1" : "0") << " "; //affiche "1" pour vivant et "0" pour mort
         }
-        cout << endl; // Saut de ligne après chaque ligne
+        cout << endl; //saut de ligne
     }
 }
 
 void Grille::initialisationGrille(vector<vector<int>>& etats_cellules) {
+    //initialise la grille afin de créer des objets correspondants à l'état d'une cellule
     for (int i = 0; i < m_longueur; i++) {
         for (int j = 0; j < m_largeur; j++) {
-            if (etats_cellules[i][j] == 1) { // Si la cellule initialisée dans le fichier d'entrée est de 1
-                cellules[i][j] = new CelluleVivante(); // Crée un nouvel objet de cellule vivante
-            } else if (etats_cellules[i][j] == 0) { // Pareil mais pour une cellule morte
+            if (etats_cellules[i][j] == 1) { //si la cellule lue dans le fichier d'entrée est de 1
+                cellules[i][j] = new CelluleVivante(); //crée un nouvel objet de cellule vivante
+            } else if (etats_cellules[i][j] == 0) { //pareil mais pour une cellule morte
                 cellules[i][j] = new CelluleMorte();
             }
         }
@@ -91,29 +94,31 @@ void Grille::initialisationGrille(vector<vector<int>>& etats_cellules) {
 
 
 vector<vector<Cellule*>> Grille::iteration() {
-    // Création d'un vecteur pour stocker l'itération suivante
+    //création d'un vecteur pour stocker l'itération suivante
     vector<vector<Cellule*>> nouvelles_cellules(m_longueur, vector<Cellule*>(m_largeur, nullptr));
-    bool changement_detecte = false; // Indicateur de changement
+    bool changement_detecte = false; //false en cas de changement, true sinon
 
     for (int i = 0; i < m_longueur; i++) {
         for (int j = 0; j < m_largeur; j++) {
             int voisins_vivants = getNBRVoisinsVivants(i, j);
 
-            if (getVieCellule(i, j)) { // La cellule est vivante
+            if (getVieCellule(i, j)) { //la cellule est vivante
                 if (voisins_vivants == 2 || voisins_vivants == 3) {
+                    //règle permettant qu'une cellule vivante reste vivante
                     nouvelles_cellules[i][j] = new CelluleVivante();
                 } else {
                     nouvelles_cellules[i][j] = new CelluleMorte();
                 }
-            } else { // La cellule est morte
+            } else { //la cellule est morte
                 if (voisins_vivants == 3) {
+                    //règle permettant à une cellule morte de devenir vivante
                     nouvelles_cellules[i][j] = new CelluleVivante();
                 } else {
                     nouvelles_cellules[i][j] = new CelluleMorte();
                 }
             }
 
-            // Vérifier s'il y a une différence avec l'état actuel
+            //vérification de différence entre l'état actuel et le prochain
             if ((getVieCellule(i, j) && dynamic_cast<CelluleMorte*>(nouvelles_cellules[i][j])) ||
                 (!getVieCellule(i, j) && dynamic_cast<CelluleVivante*>(nouvelles_cellules[i][j]))) {
                 changement_detecte = true;
